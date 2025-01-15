66a6e1632286f34f77424289ffaa8d0484de0fd7
zhangbiao218 <46077869+zhangbiao218@users.noreply.github.com>
2025-01-15 15:46:04 +0800

    Add files via upload
    初始化

---
+++ src/test/java/com/tiansuo/file/manage/ApprenticeApplicationTests.java
@@ -0,0 +1,9 @@
+package com.tiansuo.file.manage;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApprenticeApplicationTests {
+
+
+}
